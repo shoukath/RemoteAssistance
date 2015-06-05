069b435bafb46c31e5a5c7c1fefa7bb699a673a2
Shoukath Khan <shoukath.khan@orbitz.com>
2015-06-05 08:57:13 -0500

    Adding the 'less' grunt plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,11 +36,13 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('build', ['jshint']);
 
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,3 @@
  var i = 1;
  var j = 2;
+ var k = 3;
